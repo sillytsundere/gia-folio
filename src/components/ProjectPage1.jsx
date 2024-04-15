ec461c9b875d3d106c9219531805535085b1765f
Paige <paigehcarroll@gmail.com>
2024-04-15 16:11:38 -0500

    separated project title and info section into separate component

--- src/components/ProjectPage1.jsx
+++ src/components/ProjectPage1.jsx
@@ -1,24 +1,19 @@
+import ProjectTitle from "./ProjectTitle";
+
 export default function ProjectPage1({ projectData }) {
   console.log("projectData", projectData);
+  console.log("projectData image", projectData.images[0].image);
   return (
-    <div className="flex justify-center">
-      <section className="project-info w-1/2">
-        <h1>{projectData.name}</h1>
-        <hr></hr>
-        <div className="flex flex-row">
-          <div className="w-3/5">
-            <h2>Overview</h2>
-            <p>{projectData.overview}</p>
-          </div>
-          <div className="w-2/5">
-            <h2>Project Type</h2>
-            <p>{projectData.type}</p>
-            <h2>Role</h2>
-            <p>{projectData.role}</p>
-          </div>
-        </div>
+    <div className="flex flex-col items-center">
+      <ProjectTitle projectData={projectData}/>
+      <section>
+        {projectData.images.map((image) => (
+            <div key={image.image}>
+            <img src={`../${image.image}`} key={image} />
+            <p>{image.caption}</p>
+            </div>
+        ))}
       </section>
-      <section></section>
     </div>
   );
 }
