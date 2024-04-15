69f344073765e4c5ac904620064406224c37c41e
Paige <paigehcarroll@gmail.com>
2024-04-15 17:27:34 -0500

    edited page titles to be dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-
+import React, { useEffect } from 'react';
 import { Outlet, useLocation } from "react-router-dom";
 
 import Header from "./components/Header.jsx";
@@ -9,6 +9,21 @@ function App() {
   const location = useLocation();
 
   const isResumePage = location.pathname === "/resume";
+
+  useEffect(() => {
+    const currentPath = location.pathname;
+
+    const titles = {
+      '/': "Julia Pasquarella",
+      '/work': "Work - Julia Pasquarella",
+      '/resume': "Resume - Julia Pasquarella",
+      '/work/sales-pitch-design-deck' : "Sales Pitch Deck Design - Julia Pasquarella",
+      '/work/event-booth-and-collateral-design': "Event Booth And Collateral Design - Julia Pasquarella"
+    };
+
+    document.title = titles[currentPath] || "Julia Pasquarella";
+
+  }, [location]);
 
   return (
     <div className="min-h-screen">
